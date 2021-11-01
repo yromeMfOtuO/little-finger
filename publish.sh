572802fc7fc1ad763b1ad77584b755554fa25366
weihao.lv <weihao.lv@outlook.com>
2021-11-01 14:18:49 +0800

    publish script optimize

--- publish.sh
+++ publish.sh
@@ -1,19 +1,31 @@
 # TIPS：setup.py 修改版本
 
-function clean_build() {
+function clean() {
     rm -rf ./dist/
     rm -rf ./build/
     rm -rf ./little_finger.egg-info/
+    echo 'clean Successfully'
+}
+
+function build() {
+    echo 'build start ...'
+    # 构建
+    python3 setup.py sdist bdist_wheel
+    echo 'build Successfully'
+}
+
+function upload() {
+    echo 'upload start'
+    # 发布，需要输入用户名及密码
+    python3 -m twine upload dist/* -u '<username>' -p '<password>'
+    echo 'upload Successfully'
 }
 
 # 清理构建目录
-clean_build
-
-# 构建
-python3 setup.py sdist bdist_wheel
-
-# 发布，需要输入用户名及密码
-python3 -m twine upload dist/* -u <username> -p <password>
-
+echo 'clean before build ...'
+clean
+build
+upload
 # 重新清理构建目录
-clean_build
+echo 'clean after upload ...'
+clean
