d0593bc03ace86c17cbf2424c2d0e0ea8ee90937
weihao.lv <weihao.lv@outlook.com>
2021-11-16 11:32:23 +0800

    wecom client support temporary media upload and media message send

--- little_finger/nofify/wecom_client.py
+++ little_finger/nofify/wecom_client.py
@@ -12,10 +12,13 @@ TIPS:
     2. 注意部分消息类型不支持在微信上查看
     3. 图片 音频 视频 文件 等媒体消息，需要先 上传临时素材 再使用 素材id 作为消息参数发送
 """
+import enum
 import json
+import os
 
 from typing import List
 import requests
+from requests_toolbelt import MultipartEncoder
 
 
 class WecomConfig:
@@ -36,6 +39,15 @@ class WecomConfig:
     def __init__(self, corp_id: str, apps: List[WecomAppConfig]):
         self.corp_id = corp_id
         self.apps = apps
+
+
+class MediaType(enum.Enum):
+    """媒体类型"""
+
+    IMAGE = ('image',)
+    VOICE = ('voice',)
+    VIDEO = ('video',)
+    FILE = ('file',)
 
 
 class WecomClient:
@@ -78,7 +90,7 @@ class WecomClient:
         """
         for app in self.config.apps:
             resp = requests.get(
-                f"{WecomClient.bpath}/gettoken",
+                f"{self.bpath}/gettoken",
                 params=[
                     ("corpid", self.get_corp_id()),
                     ("corpsecret", app.app_secret)
@@ -86,8 +98,57 @@ class WecomClient:
             )
             app.access_token = resp.json()['access_token']
 
-    def send(self, app_id: int, user_ids: List[str], content: str,
-             part_ids: List[str] = None, tag_ids: List[str] = None):
+    def send(self, app_id, data):
+        """发送消息"""
+        resp = requests.post(
+            f'{self.bpath}/message/send',
+            params=[
+                ("access_token", self.get_access_token(app_id))
+            ],
+            data=json.dumps(data)
+        )
+        print(resp.json())
+
+    def upload(self, app_id: int, media_type: MediaType, media_path: str, media_name: str) -> str:
+        """
+        上传临时素材
+        client.upload(
+            1000002,
+            MediaType.IMAGE,
+            '/Users/weihao.lv/Desktop/壁纸/表情包',
+            'WX20210701-153912@2x.png'
+        )
+
+        TIPS：
+            1. 素材上传得到media_id，该media_id仅三天内有效
+            2. media_id在同一企业内应用之间可以共享
+        :param app_id: 应用 id
+        :param media_type: 媒体类型
+        :param media_name: 媒体文件名
+        :param media_path: 媒体文件路径
+        :return: 临时素材 id
+        """
+
+        full_path = (media_path if media_path.endswith(os.sep) else (media_path + os.sep)) + media_name
+        with open(full_path, 'rb') as media:
+            data = MultipartEncoder(
+                fields={media_name: ('file', media, 'text/plain')},
+            )
+            print(data)
+            resp = requests.post(
+                url=f"{self.bpath}/media/upload",
+                params=[
+                    ("access_token", self.get_access_token(app_id)),
+                    ("type", media_type.value[0])
+                ],
+                headers={'Content-Type': data.content_type},
+                data=data
+            )
+            print(resp.json())
+            return resp.json()['media_id']
+
+    def send_text(self, app_id: int, user_ids: List[str], content: str,
+                  part_ids: List[str] = None, tag_ids: List[str] = None):
         """
         发送文本消息
         :param user_ids: 企微中的 成员id 列表
@@ -111,21 +172,14 @@ class WecomClient:
             },
             "safe": 0
         }
-        resp = requests.post(
-            f'{WecomClient.bpath}/message/send',
-            params=[
-                ("access_token", self.get_access_token(app_id))
-            ],
-            data=json.dumps(data)
-        )
-        print(resp.json())
+        self.send(app_id, data)
 
     def send_md(self, app_id: int, user_ids: List[str], content: str,
                 part_ids: List[str] = None, tag_ids: List[str] = None):
         """
         发送文本消息
-        :param user_ids: 企微中的 成员id 列表
         :param app_id: 发送消息的 应用id
+        :param user_ids: 企微中的 成员id 列表
         :param content: 发送的文本消息内容
         :param part_ids: 部门 id 列表
         :param tag_ids: 标签 id 列表
@@ -146,14 +200,50 @@ class WecomClient:
             "enable_duplicate_check": 1,  # 标示开启重复消息检查
             "duplicate_check_interval": 1800  # 标示在时间间隔内不会收到重复消息
         }
-        resp = requests.post(
-            f'{WecomClient.bpath}/message/send',
-            params=[
-                ("access_token", self.get_access_token(app_id))
-            ],
-            data=json.dumps(data)
+        self.send(app_id, data)
+
+    def send_media(self, app_id: int, user_ids: List[str],
+                   media_type: MediaType, media_path: str, media_name: str,
+                   part_ids: List[str] = None, tag_ids: List[str] = None):
+        """
+        发送媒体消息：1.上传临时素材获取素材 id 2.发送消息
+        client.send_media(
+            1000002,
+            ['weihao.lv'],
+            MediaType.IMAGE,
+            '/Users/weihao.lv/Desktop/壁纸/表情包',
+            'WX20210701-153912@2x.png'
         )
-        print(resp.json())
+
+        :param app_id: 发送消息的 应用id
+        :param user_ids: 企微中的 成员id 列表
+        :param media_type: 媒体类型
+        :param media_path: 媒体文件路径
+        :param media_name: 媒体文件名
+        :param part_ids: 部门 id 列表
+        :param tag_ids: 标签 id 列表
+        :return: none
+        """
+        media_id = self.upload(app_id, media_type, media_path, media_name)
+
+        data = {
+            # 接受消息用户列表，为”@all”标示所有人
+            "touser": "|".join(user_ids),
+            # 部门ID列表，多个接收者用‘|’分隔，最多支持100个，当touser为”@all”时忽略
+            "toparty": "|".join(part_ids) if part_ids else None,
+            # 标签ID列表，多个接收者用‘|’分隔，最多支持100个，当touser为”@all”时忽略
+            "totag": "|".join(tag_ids) if tag_ids else None,
+            "msgtype": media_type.value[0],
+            "agentid": app_id,
+            media_type.value[0]: {
+                "media_id": media_id,
+                "title": "Title",
+                "description": "Description"
+            },
+            "enable_duplicate_check": 0,
+            "duplicate_check_interval": 1800
+        }
+        self.send(app_id, data)
 
 
 if __name__ == '__main__':
