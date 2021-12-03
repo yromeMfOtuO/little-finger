edd496ede0dc690995680080a6ee4c6c1b9f2e74
weihao.lv <weihao.lv@outlook.com>
2021-12-03 15:07:10 +0800

    wecom access_token cache by redis, just support single client, be careful

--- little_finger/notify/wecom_cache_client.py
+++ little_finger/notify/wecom_cache_client.py
@@ -5,6 +5,7 @@ import json
 
 import requests
 
+from little_finger.cache import RedisClient
 from little_finger.logg import logger
 from little_finger.notify import WecomClient, WecomConfig
 
@@ -52,6 +53,47 @@ class WecomFileCacheClient(WecomClient):
         if app.expire_time < datetime.datetime.now():
             logger.info("get access token: %s by appid: %d", app.access_token, app_id)
             return app.access_token
+        self.refresh_token()
+        return self.get_access_token(app_id)
+
+
+class WecomRedisCacheClient(WecomClient):
+    """通过 redis 缓存access_token"""
+
+    def __init__(self, config: WecomConfig, redis_client: RedisClient):
+        self.redis_client = redis_client
+        super().__init__(config)
+
+    def refresh_token(self):
+        """
+        获取 应用access_token，作为后续接口请求的凭证
+        :return: None
+        """
+        for app in self.config.apps:
+            resp = requests.get(
+                f"{self.bpath}/gettoken",
+                params=[
+                    ("corpid", self.get_corp_id()),
+                    ("corpsecret", app.app_secret)
+                ]
+            )
+            app.access_token = resp.json()['access_token']
+            self.redis_client.setex(app.access_token, '1', int(resp.json()['expires_in']))
+
+    def get_access_token(self, app_id: int):
+        """
+        获取 access token，若已失效则重新刷新获取
+        :param app_id: 应用 id
+        :return: access_token
+        """
+        if app_id not in self.app_index:
+            logger.error("illegal appid %d", app_id)
+            raise Exception("非法应用id")
+        access_token = self.app_index.get(app_id).access_token
+        # 未失效直接返回
+        if self.redis_client.get(access_token):
+            return access_token
+        # 已失效后刷新重新获取
         self.refresh_token()
         return self.get_access_token(app_id)
 
