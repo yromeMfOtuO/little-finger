cd95506cc33db52d3cb553f541aca93b458df7d3
yromeMfOtuO <weihao.lv@outlook.com>
2023-09-06 20:36:28 +0800

    add websocket listen demo

---
+++ little_finger/websocket/websocket.py
@@ -0,0 +1,27 @@
+import asyncio
+from datetime import datetime
+
+from websockets import connect
+
+
+async def listen(url, callback) -> None:
+    """
+    listen url and consumer message
+    :param url: url you want to listen
+    :param callback: consumer function to handle the message
+    :return:
+    """
+    async with connect(url) as websocket:
+        while True:
+            message = await websocket.recv()
+            if callback:
+                callback(message)
+
+
+if __name__ == '__main__':
+    asyncio.get_event_loop().run_until_complete(
+        listen(
+            "wss://stream.binance.us:9443/stream?streams=btcusdt@miniTicker/ethusdt@miniTicker",
+            lambda x: print(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + x)
+        )
+    )
