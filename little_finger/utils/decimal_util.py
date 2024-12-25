245f71b542628db25624c650ded27cd3cd95d1ce
yromeMfOtuO <weihao.lv@outlook.com>
2024-12-25 21:32:03 +0800

    add new decimal split func to split decimal to integer part and fractional part

--- little_finger/utils/decimal_util.py
+++ little_finger/utils/decimal_util.py
@@ -29,3 +29,23 @@ def to_str(num: Decimal) -> str:
     :return: str
     """
     return format(num, 'f')
+
+
+def split_decimal(num: Decimal) -> tuple[Decimal, Decimal]:
+    """
+    split decimal to integer part and fractional part
+    :param num: source num
+    :return: (integer_part_num, fractional_part_num)
+    """
+    # 将decimal转换为分数
+    numerator, denominator = num.as_integer_ratio()
+    # 分离整数部分和小数部分
+    integer_part = int(num)
+    decimal_part = abs(numerator - integer_part * denominator) / denominator
+    return Decimal(str(integer_part)), Decimal(str(decimal_part))
+
+
+if __name__ == '__main__':
+    pair = split_decimal(Decimal('3.14'))
+    print(pair[0])
+    print(pair[1])
